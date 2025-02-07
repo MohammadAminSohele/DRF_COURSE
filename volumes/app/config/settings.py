88edc2ec951581f7a0f191c4511c8bc14b65816b
MohammadAminSohele <mohammadaminsohele@gmail.com>
2025-02-07 08:23:23 +0330

    DEFAULT_PERMISSION_CLASSES

--- volumes/app/config/settings.py
+++ volumes/app/config/settings.py
@@ -131,3 +131,9 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
