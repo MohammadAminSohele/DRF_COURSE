bd3f007ef595e2843260466a7fe6bc516967f834
MohammadAminSohaili <>
2025-02-19 12:03:51 +0330

    OrderingFilter ordering ArticleViewSet

--- volumes/app/blog/views.py
+++ volumes/app/blog/views.py
@@ -15,6 +15,8 @@ class ArticleViewSet(ModelViewSet):
     serializer_class=ArticleSerializer
     filterset_fields = ['status','author__username']
     search_fields = ['title','content','author__username','author__first_name','author__last_name']
+    ordering_fields =['published','created']
+    ordering =['-published']
     def get_permissions(self):
         if self.action in ['list','create']:
             permission_classes = [IsStaffOrReadOnly]
